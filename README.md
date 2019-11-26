eee110db78ac5066172902284afb6b6541788ae0
e <zhidong10@qq.com>
2019-11-26 10:29:11 +0800

    第一版

---
+++ README.md
@@ -0,0 +1,64 @@
+# cssformatcompact README
+
+
+## Features
+
+可以实现css的两种格式化，一种是每个样式名的名称和属性格式化到一行，另外一种是每个样式名和属性单独格式化到一行，并且保留注释
+Two formats of CSS can be implemented, one is to format the name and attribute of each style name to one line, the other is to format each style name and attribute to one line separately.And keep comments.
+
+![Features](img/exp1.png)
+![Features](img/exp2.png)
+![Features](img/exp3.png)
+> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+
+## Requirements
+
+If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+
+## Extension Settings
+
+Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+
+For example:
+
+This extension contributes the following settings:
+
+* `myExtension.enable`: enable/disable this extension
+* `myExtension.thing`: set to `blah` to do something
+
+## Known Issues
+
+Calling out known issues can help limit users opening duplicate issues against your extension.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 1.0.0
+
+Initial release of ...
+
+### 1.0.1
+
+Fixed issue #.
+
+### 1.1.0
+
+Added features X, Y, and Z.
+
+-----------------------------------------------------------------------------------------------------------
+
+## Working with Markdown
+
+**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
+
+* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
+* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
+* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
+
+### For more information
+
+* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
+* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,193 @@
+import * as vscode from 'vscode';
+import { stringify } from 'querystring';
+
+function activate(context: vscode.ExtensionContext) {
+
+	console.log('Congratulations, your extension "cssformatcompact" is now active!');
+
+	let disposable = vscode.commands.registerCommand('extension.cssFormatCompact', () => {
+		cssFormatFun('cssFormatCompact');
+		//vscode.window.showInformationMessage('cssFormatCompact-样式单独一行');
+	});
+	let disposable2 = vscode.commands.registerCommand('extension.cssFormatCompactExpand', () => {
+		cssFormatFun('cssFormatCompactExpand');
+		//vscode.window.showInformationMessage('cssFormatCompactExpand-样式和属性单独一行');
+	});
+	function cssFormatFun(types: string) {
+		var editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return;
+		}
+		var selection = editor.selection;
+		var text = editor.document.getText(selection) || editor.document.getText();
+		var _this = editor;
+		editor.edit((editBuilder) => {
+			// 从开始到结束，全量替换
+			const end = new vscode.Position(_this.document.lineCount + 1, 0);
+			if (_this.document.getText(selection) == '') {//如果没有选中文本
+				editBuilder.replace(new vscode.Range(new vscode.Position(0, 0), end), cssCodeFormat.cssComm(types, text));
+			} else {
+				editBuilder.replace(selection, cssCodeFormat.cssComm(types, text));
+			}
+			// vscode.window.showInformationMessage('css格式化多行成功');
+		});
+	}
+	var cssCodeFormat = {
+		cssFormatCompact(code: string) {
+			code = code.replace(/{/ig, ' { ')											// add space before and after {
+			code = code.replace(/(@[\w-]*(document|font-feature-values|keyframes|media|supports)[^;]*?\{)\s*/ig, '$1\n');
+			// add \n after @xxx {
+
+			code = code.replace(/;/ig, '; ')											// add space after ;
+			code = code.replace(/(@(charset|import|namespace).+?;)\s*/ig, '$1\n')	// add \n after @charset & @import
+			code = code.replace(/;\s*([^\};]+?\{)/ig, ';\n$1')						// add \n before included selector
+
+			code = code.replace(/\s*(!comment!)\s*;/ig, ' $1 ;')					// fix comment before ;
+			code = code.replace(/(:[^:;]+;)\s*(!comment!)\s*/ig, '$1 $2 ')			// fix comment after ;
+
+			code = code.replace(/\s*\}/ig, ' }')										// add space before }
+			code = code.replace(/\}\s*/ig, '}\n')									// add \n after }
+
+			return code;
+		},
+		cssFormatCompactExpand(str: string) {
+			var code = str;
+			code = code.replace(/{/ig, ' {\n')									// add space before { and add \n after {
+
+			code = code.replace(/;/ig, ';\n')										// add \n after ;
+			code = code.replace(/;\s*([^\{\};]+?)\{/ig, ';\n\n$1{')				// double \n between ; and include selector
+
+			code = code.replace(/\s*(!comment!)\s*;\s*/ig, ' $1 ;\n')				// fix comment before ;
+			code = code.replace(/(:[^:;]+;)\s*(!comment!)\s*/ig, '$1 $2\n')		// fix comment after ;
+
+			code = code.replace(/\s*\}/ig, '\n}')									// add \n before }
+			code = code.replace(/\}\s*/ig, '}' + '\n\n')							// add block break after }
+			return code;
+		},
+		cssComm(action: string, str: string) {
+			// Protect comments
+			var commentReg = /[ \t]*\/\*[\s\S]*?\*\//ig
+			var comments = str.match(commentReg) || [];
+			str = str.replace(commentReg, '!comment!')
+			// Protect strings
+			var stringReg = /'(content\s*:|[\w-]+\s*=)\s*(([\'\"]).*?\3)\s*/ig
+			var strings = str.match(stringReg) || [];
+			str = str.replace(stringReg, '$1!string!')
+
+			// Protect urls
+			var urlReg = /((?:url|url-prefix|regexp)\([^\)]+\))/ig
+			var urls = str.match(urlReg) || [];
+			str = str.replace(urlReg, '!url!')
+
+			// Pre process
+			str = str.replace(/\s*([\{\}:;,])\s*/ig, '$1')	// remove \s before and after characters {}:;,
+			str = str.replace(/([\[\(])\s*/ig, '$1')			// remove space inner [ or (
+			str = str.replace(/\s*([\)\]])/ig, '$1')			// remove space inner ) or ]
+			// str = str.replace(r'(\S+)\s*([\+>~])\s*(\S+)', r'\1\2\3', code)	# remove \s before and after relationship selectors
+			str = str.replace(/,[\d\s\.\#\+>~:]*\{/ig, '{')	// remove invalid selectors without \w
+			str = str.replace(/([;,])$1+/ig, '$1')			// remove repeated ;,
+			str = str.replace(/,\s*/ig, ', ')			// add space after ,
+			// Process action rules
+			str = this.actFuns(action, str);
+			// Fix comments
+			str = str.replace(/\s*!comment!\s*@/ig, '\n\n!comment!\n@')
+			str = str.replace(/\s*!comment!\s*([^\/\{\};]+?)\{/ig, '\n\n!comment!\n$1{')
+			str = str.replace(/\s*\n!comment!/ig, '\n\n!comment!')
+
+			// Backfill comments
+			let i: any;
+			for (i in comments) {
+				str = str.replace(/[ \t]*!comment!/, comments[i]);
+			}
+
+			// Indent
+			str = this.indent_code(str)
+			// Backfill strings
+			let j: any;
+			for (j in strings) {
+				str = str.replace('!string!', strings[j][1])
+			}
+
+			// Backfill urls
+			let k: any;
+			for (k in urls) {
+				str = str.replace('!url!', urls[k])
+			}
+
+			// Trim
+			str = str.replace(/^\s*(\S+(\s+\S+)*)\s*$/ig, '$1')
+			return str;
+		},
+		actFuns(fname: any, code: string) {
+			var str: string = "";
+			if (fname == 'cssFormatCompactExpand') {
+				str = this.cssFormatCompactExpand(code)
+			} else if (fname == 'cssFormatCompact') {
+				str = this.cssFormatCompact(code)
+			}
+			return str;
+		},
+		indent_code(code: string) {
+			var lines = code.split('\n');
+			var level = 0;
+			var inComment = false;
+			var outPrefix = '';
+			var i: any;
+			for (i in lines) {
+				if (!inComment) {
+					// Quote level adjustment
+					var validCode = lines[i].replace(/\/\*[\s\S]*?\*\//ig, '')
+					validCode = validCode.replace(/\/\*[\s\S]*/ig, '')
+					var adjustment = validCode.split('{').length - validCode.split('}').length;
+
+					// Trim
+					var m = lines[i].match(/^(\s+)\/\*.*/ig);//原始 re.match(r'^(\s+)\/\*.*', lines[i]);
+					if (m) {
+						outPrefix = m[0]
+						lines[i] = lines[i].replace(RegExp('^' + outPrefix + '(.*)\s*$'), '$1')
+					} else {
+						lines[i] = lines[i].replace(/^\s*(.*)\s*$/ig, '$1');
+					}
+
+				} else {
+					// Quote level adjustment
+					adjustment = 0
+
+					// Trim
+					lines[i] = lines[i].replace(RegExp('^' + outPrefix + '(.*)\s*$'), '$1')
+				}
+				// Is next line in comment?
+				var commentQuotes = lines[i].match(/\/\*|\*\//ig)
+				var quote: any;
+				for (quote in commentQuotes) {
+					if (inComment && quote == '*/') {
+						inComment = false;
+					} else if (quote == '/*') {
+						inComment = true;
+					}
+				}
+				// Quote level adjustment
+				var nextLevel = level + adjustment
+				var thisLevel = (adjustment > 0 ? level : nextLevel)
+				level = nextLevel
+
+				// Add indentation
+				var tabs = '\t'
+				var indentation = ''
+				for (let index = 0; index < thisLevel; index++) {
+					indentation = indentation + tabs;
+				}
+				lines[i] = indentation + (lines[i] != '' ? lines[i] : '')
+
+				code = lines.join('\n')
+			}
+			return code
+		}
+	}
+	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable2);
+}
+exports.activate = activate;
+// this method is called when your extension is deactivated
+export function deactivate() { }
+//复杂的事情简单化，简单的事情重复做。
